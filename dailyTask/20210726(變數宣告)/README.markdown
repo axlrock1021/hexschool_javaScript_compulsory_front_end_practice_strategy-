80cb4a9a398c5837a4433fcc858a76556201d23f
andy <axlrock1021@hotmail.com>
2021-09-29 18:06:02 +0800

    delete file

---
+++ dailyTask/20210726(變數宣告)/README.markdown
@@ -0,0 +1,5 @@
+# 六角學院 – JavaScript 必修篇 - 前端修練全攻略 – 每日任務 2021/7/26 – (變數宣告)
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/andy1021/pen/bGqOaYP](https://codepen.io/andy1021/pen/bGqOaYP).
+
+
---
+++ dailyTask/20210726(變數宣告)/src/script.js
@@ -0,0 +1,15 @@
+// 步驟指引
+// 1.請宣告 const 變數名稱為 courseTitle ，並賦予值為字串 JavaScript 必修篇 - 前端修練全攻略。
+// 2.請宣告 let 變數名稱為 courseProgress ，並賦予值 0 。
+// 3.請宣告 const 變數名稱為 teacher ，並賦予值為字串 廖洧杰。
+// 4.請宣告 const 變數名稱為 activity ，並賦予值為字串 六角筆記王：寫作吧！用文字喚醒你的前端魂 !
+// 5.請宣告 let 變數名稱為 prize ，並賦予值為字串 小米小愛音箱 Art、米家無線吸塵器 mini、小米手環。
+
+const courseTitle = "JavaScript 必修篇 - 前端修練全攻略";
+let courseProgress = 0;
+const teacher = "廖洧杰";
+const activity = "六角筆記王：寫作吧！用文字喚醒你的前端魂！";
+let prize = "小米小愛音箱 Art、米家無線吸塵器 mini、小米手環";
+
+// 最後，觀看印出的結果
+console.log(`我參加了 ${courseTitle}課程，從 ${courseProgress} 開始認真學習，${teacher}老師舉辦了${activity}活動，還有眾多獎項：${prize}等著我!`);
