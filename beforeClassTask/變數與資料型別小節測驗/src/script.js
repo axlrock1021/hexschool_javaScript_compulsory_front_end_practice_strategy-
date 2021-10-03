ee3c3eafb5ecb2d094457f2bc6da8eed7efb8a1d
andy <axlrock1021@hotmail.com>
2021-10-04 07:40:18 +0800

    update code

---
+++ beforeClassTask/變數與資料型別小節測驗/src/script.js
@@ -0,0 +1,54 @@
+/*
+  第一題
+  請問關於變數宣告以下何者是 "錯誤" 的 ?
+  a.可以用 let、const、var 來宣告變數。
+  b.值的基本型別有 string、number、boolean、null、undefined、symbol。
+  c.let a = 1; 中文描述為宣告一個變數為 a，並賦予值為數字型別 1。
+  d.變數宣告時一定要賦予值。
+
+  ans : d； 可以先宣告變數，之後再賦予值。
+*/
+
+/*
+  第二題
+  請問有關 number 型別的描述何者為 "錯" ?
+  a.數值運算可以先乘除後加減。
+  b.number 型別能有小數點。
+  c.能用 typeof 判斷變數是否為 number 型別。
+  d.number 型別要用雙引號包覆起來。
+
+  ans : d； number 型別不用，string 型別才需要用雙引號包覆起來。
+*/
+
+/*
+  第三題
+  請問以下觀念何者為 "錯" ?
+  a.使用 alert() 可以產生彈跳式視窗。
+  b.刷新頁面時，記錄在瀏覽器的資料，會被清空。
+  c.開新分頁時，記錄在其他瀏覽器的資料，無法被讀取到。
+  d.number 型別的數值無法為負數。
+
+  ans : d； 可以為負數。
+*/
+
+/*
+  第四題
+  小明宣告了兩個變數 a、b ，並各賦予值為 4、2，如果小明希望宣告變數 c 為 a 的 b 次方，該如何撰寫 c ?
+  a.let c = a % b;
+  b.let c = a / b;
+  c.let c = a ** b;
+  d.let c = a * b;
+
+  ans : c。
+*/
+
+/*
+  第五題
+  小美去超商買了兩杯紅茶和一顆漢堡，紅茶和漢堡各為 20 元、40 元，請問以下計算過程何者 "正確" ?
+  a.let blackTeaPrice = 20; let hamburgerPrice = 40; let total = (blackTeaPrice * 2) + (hamburgerPrice * 1);
+  b.let blackTeaPrice = 30; let hamburgerPrice = 40; let total = (blackTeaPrice * 1) * (hamburgerPrice * 1);
+  c.let blackTeaPrice = 10; let hamburgerPrice = 40; let total = (blackTeaPrice * 1) - (hamburgerPrice * 1);
+  d.let blackTeaPrice = 10; let hamburgerPrice = 40; let total = (blackTeaPrice * 2) / (hamburgerPrice * 1);
+
+  ans : a。
+*/
