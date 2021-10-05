1e2d5bfd0f41cd59d05974890b7bd20a7a0b3986
andy <axlrock1021@hotmail.com>
2021-10-05 10:23:46 +0800

    update code

---
+++ beforeClassTask/F.變數：布林、undefined、null 小節測驗/README.markdown
@@ -0,0 +1,5 @@
+# 六角學院 – JavaScript 必修篇 – 前端修練全攻略 – 課前 - 變數：布林、undefined、null 小節測驗
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/andy1021/pen/oNwrxKY](https://codepen.io/andy1021/pen/oNwrxKY).
+
+
---
+++ beforeClassTask/F.變數：布林、undefined、null 小節測驗/src/script.js
@@ -0,0 +1,59 @@
+/*
+  第一題
+  關於布林的介紹何者 "錯誤" ?
+  a.布林要用單引號包覆起來。
+  b.布林只有兩個值 true 、 false。
+  c.console.log(2 > 3) 的回傳結果是 false。
+  d.true 為正確、false 為不正確。
+
+  ans : a； 不用，字串才需要。
+*/
+
+/*
+  第二題
+  關於 undefined 的介紹何者 "錯誤" ?
+  a.undefined 表示尚未被賦予值的狀態。
+  b.僅宣告變數未賦予變數值時該變數值為 undefined。
+  c.undefined 的型別為 undefined。
+  d.undefined 的型別為 string。
+
+  ans : d； undefined 型別就是 undefined。
+*/
+
+/*
+  第三題
+  關於 null 的介紹何者 "錯誤" ?
+  a.null 跟 undefined 沒有不同。
+  b.null 代表有被賦予值，被賦予了空值。
+  c.想清空資料時可以使用 null。
+  d.使用 null 清空資料，可以釋放記憶體空間。
+
+  ans : a; null 有被賦予值，被賦予了空值，undefined 尚未被賦予值。
+*/
+
+/*
+  第四題
+  以下敘述何者 "錯誤" ?
+  a.可以使用 parseInt() 將字串轉型為數字。
+  b.從表單文字取出來的數字都會是字串。
+  c.parseInt("你很棒")結果為 NaN。
+  d.NaN 可以拿來做運算。
+
+  ans : d; 不能拿來做運算。 
+*/
+
+/*
+  第五題
+  請參考以下程式碼，選出 a 、 b 正確的值跟型別
+  let a = "1";
+  let b = 3;
+  b = b.toString();
+  a += b;
+  a = parseInt(a);
+  a.a = number 13 、 b = string "3"。
+  b.a = string "13" 、 b = number 3。
+  c.a = string "3" 、 b = number 13。
+  d.a = number 3 、 b = string "13"。
+
+  ans : a;
+*/
