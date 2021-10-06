44746fecc5a9464353660b26f2b75d5745c6c0c0
andy <axlrock1021@hotmail.com>
2021-10-06 20:16:25 +0800

    update code

---
+++ beforeClassTask/H.比較與邏輯運算子小節測驗/javascript/README.markdown
@@ -0,0 +1,5 @@
+# 六角學院 – JavaScript 必修篇 – 前端修練全攻略 – 課前 - 比較與邏輯運算子小節測驗
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/andy1021/pen/QWgeEmo](https://codepen.io/andy1021/pen/QWgeEmo).
+
+
---
+++ beforeClassTask/H.比較與邏輯運算子小節測驗/javascript/src/script.js
@@ -0,0 +1,72 @@
+/*
+  第一題
+  關於比較運算子的敘述何者 "錯誤" ?
+  a.比較運算子包含 > 、 >= 、 < 、 <= 、 == 、!=。
+  b.可以利用比較運算子比較數值。
+  c.比較運算子只能比較數值。
+  d.比較運算子可以搭配變數使用。
+
+  ans : c; 也可搭配變數使用。
+*/
+
+/*
+  第二題
+  以下關於 = 、 == 、 === 的敘述何者為 "錯" ?
+  a.一個 = 代表賦予值。
+  b.== 是比較運算子。
+  c.=== 不會比較型別。
+  d.== 比 === 不嚴謹。
+
+  ans : c； 最嚴謹，會比較型別跟值。
+*/
+
+/*
+  第三題
+  關於邏輯運算子的敘述何者 "錯誤" ?
+  a.邏輯運算子包含 && 、 ||。
+  b.&& 代表同時滿足條件。
+  c.|| 代表滿足其中一個條件。
+  d.邏輯運算子不可以多條件使用，一次只能使用一組。
+
+  ans : d; 可以多條件使用。
+*/
+
+/*
+  第四題
+  請問以下 console.log 各會回傳什麼 ?
+  // && 練習
+  let a = true;
+  let b = false;
+  console.log(a === true && b === true);
+  // || 練習
+  let c = true;
+  let d = false;
+  console.log(c === true || d === true);
+  a.皆為 true。
+  b.皆為 false。
+  c.&& 練習為 true 、 || 練習為 false。
+  d.&& 練習為 false 、 || 練習為 true。
+
+  ans : d;
+*/
+
+/*
+  第五題
+  請問以下 console.log 各會回傳什麼 ?
+  // && 多組練習
+  let a = true;
+  let b = true;
+  let c = false;
+  console.log(a === true && b === true && c === true);
+  // || 多組練習
+  let d = true;
+  let e = true;
+  let f = false;
+  console.log(d === true || e === true || f === true);
+  a.皆為 true。
+  b.皆為 false。
+  c.&& 練習為 true 、 || 練習為 false。
+  d.&& 練習為 false 、 || 練習為 true。
+
+  ans : d;
+*/
