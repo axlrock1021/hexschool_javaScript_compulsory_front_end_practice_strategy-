b921c165e1fde09cd4fed3513e77f64ec1104bcb
andy <axlrock1021@hotmail.com>
2021-10-26 09:07:11 +0800

    update code

---
+++ dailyTask/20210809(JSON 格式判斷)/src/script.js
@@ -0,0 +1,63 @@
+/*
+  題目一 
+  參考範例 
+  let mingStatus = [
+    {
+      name: "小明",
+      age: "21",
+      male: "男",
+      isPass: true
+    }
+  ];
+  回答舉例 ： 最外層是一個陣列，陣列內包含很多個物件，物件內有 … 屬性，值為 … 。
+  最外層是一個陣列，陣列內包含一個物件。
+  物件內有 "name" 、 "age" 、 "male" 、 "isPass" 屬性。
+  屬性的值分別為 "小明" 、 "21" 、 "男" 、 true。
+
+  每月盛產農產品產地資料網址 ： https://data.coa.gov.tw/Service/OpenData/DataFileService.aspx?UnitId=061
+  let agriculturalProductsData = [
+    {
+      type: "水果",
+      month: "1",
+      crop: "椪柑",
+      variety: "",
+      county: "苗栗縣",
+      town: "卓蘭鎮",
+    },
+    {
+      type: "水果",
+      month: "1",
+      crop: "椪柑",
+      variety: "",
+      county: "台中市",
+      town: "東勢區",
+    }
+  ];
+  最外層是一個陣列，陣列內包含很多個物件。
+  物件內有 "type" 、 "month" 、 "crop" 、 "variety" 、 "county" 、 "town" 屬性。
+  值為 "水果" 、 "1" 、 "椪柑" 、 "" 、 "苗栗縣" 、 "卓蘭鎮"。
+
+
+  題目二
+  老人服務中心資料網址 ： https://data.kcg.gov.tw/dataset/9980b8b3-a64d-4ec8-9e31-c91a086f4ea7/resource/6641a557-b500-4652-b540-7085d5021936/download/-.json
+  let data = {
+    orgs: {
+      frg: {
+        org: {
+          id: "89954",
+          lat: "22.584618",
+          lng: "120.312329",
+          informaddress: "高雄市前鎮區興化街與千富街交界",
+          hlink: "",
+          informtel: "2518200",
+          servItem: "老人文康休閒服務",
+          servTime: "星期一至星期五08：00-17：00",
+          dataOrg: "老人福利科",
+          doOrg: "高雄市社區大學促進會",
+          text: "南區銀髮族市民農園",
+        },
+        id: "FRG0610",
+      },
+    },
+  };
+*/
